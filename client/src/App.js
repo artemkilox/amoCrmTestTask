29ec52a8044abbf522a8b1c1ec630bcb74f27763
artemkilox <artemchernykh001@mail.ru>
2024-02-09 01:25:54 +0300

    asd

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import MainPage from "./components/mainPage";
+import React from "react";
+
+const App = () => {
+  return (
+      <div>
+        <MainPage />
+      </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/mainPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MainPage = () => {
+    const secretKey = '0xzzCR3OSugE53hwYevnfSCVWoT6cZelACKF73lWvlXdKaieojkALiTyOiHn8e8D'
+    const integrationID = 'cc4411e0-56e0-4b0f-a9b0-5d2d59081e58'
+
+    const accessToken = ''
+    const refreshToken = ''
+
+    // https://www.amocrm.ru/oauth?client_id={Integration ID}
+    //     // &state={параметр состояния, который будет возвращен вам на Redirect URI}
+    //     // &mode={popup или post_message}.
+    return (
+        <div>
+            main
+        </div>
+    );
+};
+
+export default MainPage;
